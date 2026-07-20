3c1d5a82adaf46b7184782ee774b4aeb50f7b152
Aryaman Dalal <ad515@duke.edu>
2026-07-20 13:37:54 +0530

    Relocate IOI_Replication to research/P1 (submodule root); reset tracking
    Files will be re-added individually via 'git add -f'. Prior history is
    preserved in the log (commits dfa820b, 17a4d9c, f9c51a2).

--- direct_logit_attribution.py
+++
@@ -1,9 +0,0 @@
-import torch
-from ioi_prompts import ioi_prompts
-import verify_offline
-
-torch.set_grad_enabled(False)
-model = verify_offline.gpt2()
-prompt = next(ioi_prompts())
-
-print(prompt)
